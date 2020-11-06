61f6d91bafa31887c520455eeb75b7ca6c846014
Anand <anands.kartha@gmail.com>
2020-11-06 00:57:37 -0500

    Squashed commit of the following:
    commit e75a22e346db715b7ffb9f2cb77932effdf412b4
    Author: Anand <anands.kartha@gmail.com>
    Date:   Fri Nov 6 00:52:53 2020 -0500
    
        About page changes
    
    commit 9612e93cde127ce7ad0b7e7001c0f86e539f15f3
    Author: Anand <anands.kartha@gmail.com>
    Date:   Fri Sep 4 13:21:38 2020 +0530
    
        Error boundary added

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -8,6 +8,7 @@ import Sidebar from '../Template/Sidebar';
 import Spinner from '../Template/Spinner'
 import Dialog from '../Template/Dialog'
 import Login from '../Login'
+import ErrorBoundary from '../Error/ErrorBoundary';
 
 import TodoListApp from '../TodoListApp';
 import ExpenseTracker from '../ExpenseTracker';
@@ -44,36 +45,38 @@ class App extends React.Component {
                                     </nav>
                                 </div>
                             </header>
-                                <Switch>
-                                    <Route exact path='/'>
-                                        <Showcase header="Javascript Applications" content="Checkout a few sample applications and utilities I developed using some of the most popular web development frameworks. These are not my best, but I just want to show off my work to the world. Have fun and tell me what you think!"/>
-                                        <HomeContent/>
-                                    </Route>
-                                    <Route exact path='/todoApp'>
-                                        <div className="main-content">
-                                            <TodoListApp/>
-                                            <Sidebar title="Todo App" content="Manage a list of TODO activities that can be striked out after the task is completed. The React component uses component states and props along with Express server and MongoDB." />
-                                        </div>
-                                    </Route>
-                                    <Route exact path='/expenseTracker'>
-                                        <div className="main-content">
-                                            <ExpenseTracker />
-                                            <Sidebar title="Expense Tracker" content="Want to keep track of all your incomes and expenses? This utility uses React Context and Hooks which is a combination that gives developers ability to manage and share application level states among components like the way they do in Redux. We can manage a shared state among components without being passed it to child components as props." />
-                                        </div>
-                                    </Route>
-                                    <Route exact path='/about'>
-                                        <Showcase header="A little bit about me" content="" btnTitle="Say Hi!" bgColor="#212121" btnClickAction={() => ref.current.scrollIntoView({behavior: 'smooth'})}/>
-                                        <div className="main-content">
-                                            <About contactRef={ref}/>
-                                        </div>
-                                    </Route>
-                                </Switch>
+                                <ErrorBoundary>
+                                    <Switch>
+                                        <Route exact path='/'>
+                                            <Showcase header="Javascript Applications" content="Checkout a few sample applications and utilities I developed using some of the most popular web development frameworks. These are not my best, but I just want to show off my work to the world. Have fun and tell me what you think!"/>
+                                            <HomeContent/>
+                                        </Route>
+                                        <Route exact path='/todoApp'>
+                                            <div className="main-content">
+                                                <TodoListApp/>
+                                                <Sidebar title="Todo App" content="Manage a list of TODO activities that can be striked out after the task is completed. The React component uses component states and props along with Express server and MongoDB." />
+                                            </div>
+                                        </Route>
+                                        <Route exact path='/expenseTracker'>
+                                            <div className="main-content">
+                                                <ExpenseTracker />
+                                                <Sidebar title="Expense Tracker" content="Want to keep track of all your incomes and expenses? This utility uses React Context and Hooks which is a combination that gives developers ability to manage and share application level states among components like the way they do in Redux. We can manage a shared state among components without being passed it to child components as props." />
+                                            </div>
+                                        </Route>
+                                        <Route exact path='/about'>
+                                            <Showcase header="A little bit about me" content="" btnTitle="Say Hi!" bgColor="#212121" btnClickAction={() => ref.current.scrollIntoView({behavior: 'smooth'})}/>
+                                            <div className="main-content">
+                                                <About contactRef={ref}/>
+                                            </div>
+                                        </Route>
+                                    </Switch>
+                                </ErrorBoundary>
                             <Footer/>
                             <Spinner />
                             <Dialog />
                         </Router>
                 </AuthProvider>
-            </Provider>
+            </Provider>            
         );
     }
 }
